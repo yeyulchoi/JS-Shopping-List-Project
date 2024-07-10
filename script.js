b244f82c787b17a9f8812ef41e777e996b28edbd
yeyulchoi <yeyulchoi@outlook.com>
2024-07-10 03:20:29 -0400

    created filter function

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const itemForm=document.getElementById('item-form');
 const itemInput=document.getElementById('item-input');
 const itemList=document.getElementById('item-list');
-
 const clearBtn=document.getElementById('clear');
 const filter=document.getElementById('filter');
 
-
-
-
-
-
-
-
+// console.log(itemList.children);// this is HTMLCollection. It is not a list...
 
 function addItem(e){
     e.preventDefault();  //cos not actually submit in this app.
@@ -53,12 +46,9 @@ function addItem(e){
 function removeItem(e){
     if(e.target.classList.contains('remove-item') || e.target.parentElement.classList.contains('remove-item')){
        if(confirm("Are you sure")){
-        e.target.closest('li').remove();
-       }
-       
-             
-     }
-     checkUI();
+        e.target.closest('li').remove()
+        checkUI()
+    }}    
 }
 function clearAll(){
    while(itemList.firstChild){
@@ -68,9 +58,28 @@ function clearAll(){
     checkUI();
 }
 
+function filterItems(e){
+    const text=e.target.value.toLowerCase();
+    const items= itemList.querySelectorAll('li'); 
+    items.forEach(item=>{
+        const itemName=item.firstChild.textContent.toLowerCase();
+        
+        if(itemName.indexOf(text)!==-1){
+            item.style.display='flex';
+            console.log(true);
+        }else{
+            item.style.display='none';
+            console.log(false);
+        }
+
+        
+    })
+    console.log(text);
+}
 function checkUI(){
-    const items= itemList.querySelectorAll('li');
-    console.log(items);
+    const items= itemList.querySelectorAll('li');  //note: if using ...tagName('li')which is old style
+    // console.log(items);                                              //then wrap it with Array.from() since it is HTMLCollection, not nodelist
+   
     if (items.length===0){
         filter.style.display='none';
         clearBtn.style.display='none';
@@ -81,11 +90,16 @@ function checkUI(){
 
 }
 
+
+
 checkUI();
+
+//Event Listener
 itemForm.addEventListener('submit',addItem);
 // remove Item method- 2: using classList
 itemList.addEventListener('click',removeItem)
 clearBtn.addEventListener('click',clearAll)
+filter.addEventListener('input', filterItems);
 
 
 // remove Item method- 1: using tagName
@@ -104,7 +118,3 @@ clearBtn.addEventListener('click',clearAll)
 //     }
 // })
 
-//clear all items
-
-
-   
