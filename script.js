e78c22f22442f59c1e3ec4bef6c1d79de2303bc0
yeyulchoi <yeyulchoi@outlook.com>
2024-07-11 00:01:30 -0400

    set item to edit & update and reset state

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput=document.getElementById('item-input');
 const itemList=document.getElementById('item-list');
 const clearBtn=document.getElementById('clear');
 const filter=document.getElementById('filter');
+const formBtn =itemForm.querySelector('button');
+let isEditMode = false;
 
 // console.log(itemList.children);// this is HTMLCollection. It is not a list...
 
@@ -23,9 +25,21 @@ function onAddItemSubmit(e){
         alert("Please add an item")
         return; // return=> because we dont want anything to happen.
     }
+
+    //check for edit mode
+    if(isEditMode){
+        const itemToEdit =itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode=false;
+
+    }
+    
     addItemToDOM(newItem)
     addItemToStorage(newItem)
-    displayLocalData()
+    // displayLocalData()
 
 
     checkUI();
@@ -85,8 +99,22 @@ function getItemsFromStorage(){
 function onClickItem(e){
     if(e.target.parentElement.classList.contains('remove-item')){
         removeItem(e.target.parentElement.parentElement);
+    }else{
+        setItemToEdit(e.target);
+        
     }
    }
+
+function setItemToEdit(item){
+    isEditMode =true;
+    
+    itemList.querySelectorAll('li').forEach(i=>i.classList.remove('edit-mode'))
+    item.classList.add('edit-mode');
+    formBtn.innerHTML='<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.style.backgroundColor='#228B22';
+    itemInput.value = item.textContent;
+
+}
 
 function removeItem(item){
     if(confirm('Are you Sure?')){
@@ -123,7 +151,6 @@ function clearAll(){
 }
 
 
-
 function filterItems(e){
     const text=e.target.value.toLowerCase();
     const items= itemList.querySelectorAll('li'); 
@@ -137,12 +164,13 @@ function filterItems(e){
             item.style.display='none';
             // console.log(false);
         }
-
         
     })
     // console.log(text);
 }
 function checkUI(){
+
+    itemInput.value='';
     const items= itemList.querySelectorAll('li');  //note: if using ...tagName('li')which is old style
     // console.log(items);                                              //then wrap it with Array.from() since it is HTMLCollection, not nodelist
    
@@ -153,6 +181,10 @@ function checkUI(){
         filter.style.display='block';
         clearBtn.style.display='block';
     }
+
+    formBtn.innerHTML='<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor='#333';
+    isEditMode=false;
 
 }
 
